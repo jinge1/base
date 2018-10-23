9289931ba56fd339c5196b2bf39d68902a36ec42
lixiong <lixiong2@niiwoo.com>
2018-10-23 17:53:23 +0800

    update

---
+++ frontEnd/vueBase/bin/webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    publicPath: './'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.vue']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'style-loader', 'css-loader', 'postcss-loader']
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({filename: 'index.html', template: 'src/index.html'}),
+    new VueLoaderPlugin()
+  ]
+}
