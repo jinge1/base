39e336b1da37474e274588b5410c389c7331bef8
lixiong <346959251@qq.com>
2018-12-26 23:34:42 +0800

    a

--- frontEnd/tsVue/bin/webpack.dev.js
+++ frontEnd/tsVue/bin/webpack.dev.js
@@ -43,7 +43,7 @@ module.exports = {
       test: /\.js$/,
       use: ['babel-loader']
     }, {
-      test: /\.css$/,
+        test: /\.(css|postcss)$/,
       use: [
       'style-loader', 
       'css-loader', 
--- frontEnd/tsVue/src/components/Index.vue
+++ frontEnd/tsVue/src/components/Index.vue
@@ -1,43 +1,41 @@
 <template>
-<div>
-  <div class="test2">
-    <button @click="mAddNum(-3)">a add</button>
-    <span>{{count}}</span>
-    <button @click="addNum(2)">m add</button>
+  <div>
+    <div class="test2">
+      <button @click="mAddNum(-3)">a add</button>
+      <span>{{count}}</span>
+      <button @click="addNum(2)">m add</button>
+    </div>
+    <p>{{msg}}</p>
   </div>
-  <p>{{msg}}</p>
-</div>
-
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import { State, Action, Mutation, Getter } from 'vuex-class'
+import Vue from "vue";
+import Component from "vue-class-component";
+import { State, Action, Mutation, Getter } from "vuex-class";
 
 @Component
 export default class Counter extends Vue {
-  @State count: number
-  @Getter msg: string
-  @Mutation('ADD_NUM') addNum: void
-  @Action('ADD_NUM') mAddNum: void
+  @State count: number;
+  @Getter msg: string;
+  @Mutation("ADD_NUM") addNum: void;
+  @Action("ADD_NUM") mAddNum: void;
   created () {
-    console.log('ok?')
+    console.log("ok?");
   }
-
 }
 </script>
-<style scoped>
-.test2{
-  display:flex;
-  span{
-    flex:1;
+<style lang="postcss" scoped>
+.test2 {
+  display: flex;
+  span {
+    flex: 1;
   }
-  button{
-    width:3rem;
-    line-height:3em;
-    background:orange;
-    color:#fff;
+  button {
+    width: 3rem;
+    line-height: 3em;
+    background: orange;
+    color: #fff;
   }
 }
 </style>
